195ea676f919bfb59828e5472dd6d474d4e7787d
shrub <shrub719@gmail.com>
2025-03-20 19:56:33 +0000

    reformatted picker

--- src/picker.py
+++ src/picker.py
@@ -1,13 +1,16 @@
 from kandinsky import *
 from ion import *
 
+
 def draw(colour):
     fill_rect(0, 0, 320, 200, color(tuple(colour)))
+
 
 def show(colour):
     draw_string(str(colour[0]), 0, 200)
     draw_string(str(colour[1]), 50, 200)
     draw_string(str(colour[2]), 100, 200)
+
 
 def listen(key, colour, value, changeBy):
     if keydown(key):
@@ -17,6 +20,7 @@ def listen(key, colour, value, changeBy):
         elif colour[value] < 0:
             colour[value] = 0
         draw(colour)
+
 
 colour = [0, 0, 0]
 draw(colour)
