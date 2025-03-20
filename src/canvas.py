7b6eaf7dce7b3ef3001fb2035571da055fab717a
shrub <shrub719@gmail.com>
2025-03-20 20:17:50 +0000

    renamed Cursor.colour to Cursor.covering

--- src/canvas.py
+++ src/canvas.py
@@ -9,6 +9,7 @@ HEIGHT = 222
 def invertColor(colour):
     return tuple(255 - value for value in colour)
 
+
 def moveListen(key, vector, cursor):
     if keydown(key):
         t = 0.02
@@ -21,20 +22,20 @@ class Cursor:
     def __init__(self):
         self.x = WIDTH //  2
         self.y = HEIGHT // 2
-        self.colour = (255, 255, 255)  # keeps track of what the cursor is currently covering
+        self.covering = (255, 255, 255)  # keeps track of what the cursor is currently covering
         self._draw()
 
     def _draw(self):
-        set_pixel(self.x, self.y, invertColor(self.colour))
+        set_pixel(self.x, self.y, invertColor(self.covering))
 
     def _undraw(self):
-        set_pixel(self.x, self.y, self.colour)
+        set_pixel(self.x, self.y, self.covering)
 
     def move(self, vector):
         self._undraw()
         self.x += vector[0]
         self.y += vector[1]
-        self.colour = get_pixel(self.x, self.y)
+        self.covering = get_pixel(self.x, self.y)
         self._draw()
 
 
