fb79b994a17e2af033e9bf50050ce0fbbfb56436
shrub <shrub719@gmail.com>
2025-03-20 20:26:42 +0000

    edited some variables

--- src/canvas.py
+++ src/canvas.py
@@ -4,6 +4,9 @@ from time import sleep
 
 WIDTH = 320
 HEIGHT = 222
+SLOW = 0.01
+FAST = 0.002
+diff = SLOW - FAST
 
 
 def invertColor(colour):
@@ -12,10 +15,9 @@ def invertColor(colour):
 
 def moveListen(key, vector, cursor):
     if keydown(key):
-        t = 0.02
-        if keydown(KEY_ONE): t = 0.005
         cursor.move(vector)
-        sleep(t)
+        sleep(FAST)
+        if not keydown(KEY_TOOLBOX): sleep(diff)
 
 
 class Cursor:
@@ -48,4 +50,4 @@ while True:
     moveListen(KEY_RIGHT, (1, 0), cursor)
     moveListen(KEY_UP, (0, -1), cursor)
     moveListen(KEY_DOWN, (0, 1), cursor)
-    if keydown(KEY_TWO): cursor.draw((255, 0, 0))
+    if keydown(KEY_OK): cursor.draw((255, 0, 0))
