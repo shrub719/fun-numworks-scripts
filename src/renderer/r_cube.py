955d061f82f6ba99809f0af54e7ad4dfba5ef4aa
shrub <shrub719@gmail.com>
2025-04-21 17:36:47 +0100

    added face logic

--- src/renderer/r_cube.py
+++ src/renderer/r_cube.py
@@ -6,4 +6,18 @@ for i in range(8):
     point = tuple(x * 2 - 1 for x in point)
     cube.append(point)
 
-OBJECT = cube
+faces = [
+    ([0, 1, 2, 3], "black"),
+    ([4, 5, 6, 7], "black"),
+    ([0, 1, 4, 5], "black"),
+    ([2, 3, 6, 7], "black"),
+    ([0, 2, 4, 6], "black"),
+    ([1, 3, 5, 7], "black")
+]
+
+OBJECT = cube   # list of points, as coordinates
+FACES = faces   # list of faces, as indexes in POINTS
+
+if __name__ == "__main__":
+    print(OBJECT)
+    print(FACES)
