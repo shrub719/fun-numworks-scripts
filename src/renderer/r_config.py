c08245296b12fac313140e043b6f06017421d250
shrub <shrub719@gmail.com>
2025-04-24 20:10:49 +0100

    made colours/objects autocorrect

--- src/renderer/r_config.py
+++ src/renderer/r_config.py
@@ -1,22 +1,21 @@
 from math import sin, cos
 
-cube = []
+O_cube = []
 for a in [-1, 1]:
     for b in [-1, 1]:
         c = -1
         for i in range(5):
-            cube.append((a, b, c))
-            cube.append((a, c, b))
-            cube.append((c, a, b))
+            O_cube.append((a, b, c))
+            O_cube.append((a, c, b))
+            O_cube.append((c, a, b))
             c += 0.5
 
+O_cuboid = [(point[0] * 2, point[1], point[2]) for point in O_cube]
 
-cuboid = [(point[0] * 2, point[1], point[2]) for point in cube]
+O_rings = [(sin(t*0.2), cos(t*0.2), 0) for t in range(31)] + [(0, sin(t*0.2), cos(t*0.2)) for t in range(31)]
+O_rings2 = [(sin(t*0.4), cos(t*0.4), 0) for t in range(16)] + [(0, sin(t*0.4), cos(t*0.4)) for t in range(16)] + [(sin(t*0.4), 0, cos(t*0.4)) for t in range(16)]
 
-rings = [(sin(t*0.2), cos(t*0.2), 0) for t in range(31)] + [(0, sin(t*0.2), cos(t*0.2)) for t in range(31)]
-rings2 = [(sin(t*0.4), cos(t*0.4), 0) for t in range(16)] + [(0, sin(t*0.4), cos(t*0.4)) for t in range(16)] + [(sin(t*0.4), 0, cos(t*0.4)) for t in range(16)]
-
-pyramid = [
+O_pyramid = [
     (-1, -1, -1), 
     (-1, -1, 1),
     (1, -1, -1),
@@ -24,8 +23,10 @@ pyramid = [
     (0, 1.4, 0)
 ]
 
-grey = (0.8, 0.8, 0.8)   # TODO: TEST
-orange = (1, 0.7, 0.2)
+
+C_grey = (0.8, 0.8, 0.8)
+C_orange = (1, 0.7, 0.2)
+C_purple = (0.5, 0.2, 1)
 
 
 # ===== CONFIG =====
@@ -34,8 +35,8 @@ orange = (1, 0.7, 0.2)
 IN_PLACE = True
 SPEED = 0.02
 
-# options: grey / orange
-COLOUR = grey
+# options: grey / orange / purple
+COLOUR = C_purple
 
 # options: cube / cuboid / rings / rings2
-OBJECT = cube
+OBJECT = O_cube
