60fe9d1822ed2c263cf72306d2275b906290fe51
shrub719 <shrub719@gmail.com>
2025-04-25 09:26:49 +0100

    ported r_config to numpy

--- src/renderer/r_config.py
+++ src/renderer/r_config.py
@@ -1,34 +1,56 @@
 from math import sin, cos
+import numpy as np  # TODO: add to requirements.txt
 
-O_cube = []
+O_cube = np.empty((0, 3))
 for a in [-1, 1]:
     for b in [-1, 1]:
         c = -1
         for i in range(5):
-            O_cube.append((a, b, c))
-            O_cube.append((a, c, b))
-            O_cube.append((c, a, b))
+            O_cube = np.append(O_cube, [[a, b, c]], axis=0)
+            O_cube = np.append(O_cube, [[a, c, b]], axis=0)
+            O_cube = np.append(O_cube, [[c, a, b]], axis=0)
             c += 0.5
 
-O_cuboid = [(point[0] * 2, point[1], point[2]) for point in O_cube]
+O_cuboid = None # TODO: vectorise or whatever
 
-O_rings = [(sin(t*0.2), cos(t*0.2), 0) for t in range(31)] + [(0, sin(t*0.2), cos(t*0.2)) for t in range(31)]
-O_rings2 = [(sin(t*0.4), cos(t*0.4), 0) for t in range(16)] + [(0, sin(t*0.4), cos(t*0.4)) for t in range(16)] + [(sin(t*0.4), 0, cos(t*0.4)) for t in range(16)]
-O_donut = [(sin(t*0.2), cos(t*0.2), 0) for t in range(31)]
-O_donut = O_donut + [(x, y, z+0.3) for x, y, z in O_donut]
+O_rings = np.concatenate(
+    (
+        [(sin(t*0.2), cos(t*0.2), 0) for t in range(31)],
+        [(0, sin(t*0.2), cos(t*0.2)) for t in range(31)]
+    ),
+    axis=0
+)
 
-O_pyramid = [
-    (-1, -1, -1), 
-    (-1, -1, 1),
-    (1, -1, -1),
-    (1, -1, 1),
-    (0, 1.4, 0)
-]
+
+O_rings2 = np.concatenate(
+    (
+        [(sin(t*0.4), cos(t*0.4), 0) for t in range(16)],
+        [(0, sin(t*0.4), cos(t*0.4)) for t in range(16)],
+        [(sin(t*0.4), 0, cos(t*0.4)) for t in range(16)]
+    ),
+    axis=0
+)
+
+O_donut = np.concatenate(
+    (
+        [(sin(t*0.2), cos(t*0.2), 0.2) for t in range(31)],
+        [(sin(t*0.2), cos(t*0.2), -0.2) for t in range(31)]
+    ),
+    axis=0
+)
 
 
 C_grey = (0.8, 0.8, 0.8)
 C_orange = (1, 0.7, 0.2)
 C_purple = (0.5, 0.2, 1)
+
+
+if __name__ == "__main__":
+    print("CUBE", O_cube)
+    print("CUBOID", O_cuboid)
+    print("RINGS", O_rings)
+    print("RINGS2", O_rings2)
+    print("DONUT", O_donut)
 
 
 # ===== CONFIG =====
